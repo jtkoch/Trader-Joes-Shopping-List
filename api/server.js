54ea061b913d5cd8d04143273f4b3bae6e4ddc04
Jensen Koch <jtkoch12@gmail.com>
2020-06-05 20:33:46 -0500

    Added authorization, register, login, restrict

--- api/server.js
+++ api/server.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const helmet = require("helmet")   
 const cors = require("cors")
-//////////////////// const restricted = require("../auth/restricted.js")
+const restricted = require("../auth/restricted.js")
 
 //routers
 const usersRouter = require("../users/users-router.js")
-//////////////////// const authRouter = require("../auth/auth-router.js")
+const authRouter = require("../auth/auth-router.js")
 const itemsRouter = require("../items/items-router.js")
 
 const server = express()
@@ -16,9 +16,9 @@ server.use(cors())
 server.use(helmet())
 
 //routes
-server.use("/api/users", logger, usersRouter)
-//////////////////// server.use("/api/auth", logger, authRouter)
-server.use("/api/items", logger, itemsRouter)
+server.use("/api/users", logger, restricted, usersRouter)
+server.use("/api/auth", logger, authRouter)
+server.use("/api/items", logger, restricted, itemsRouter)
 
 server.get("/", (req, res) =>{
     res.status(200).json({message: "Welcome to the Trader Joe's Shopping List API"})
