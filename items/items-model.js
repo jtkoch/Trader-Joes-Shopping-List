445e3f4e3715e7c0c9419f0766e205eca5bc2686
Jensen Koch <jtkoch12@gmail.com>
2020-06-24 11:26:40 -0500

    refactored items-model.js

--- items/items-model.js
+++ items/items-model.js
@@ -1,44 +1,87 @@
 const db = require("../data/dbConfig")
 
-function find(){
+// function find(){
+//   return db("items")
+//   .select("name", "price", "category")
+// }
+
+// function findBy(filter) {
+//   return db("items").where(filter)
+// }
+
+// function findById(id) {
+//   return db("items")
+//   .select("item_id", "name", "price", "category")
+//   .where({id})
+//   .first()
+// }
+
+// async function insert(item) {
+//   return await db("items")
+//   .insert(item)
+//   .returning(["id", "name"])
+// }
+
+// function remove(id) {
+//   return db("items")
+//   .where({id})
+//   .del()
+// }
+
+// function update(changes, id) {
+//   return db("items")
+//   .where({id})
+//   .update(changes)
+// }
+
+// module.exports = {
+//   find, 
+//   findBy, 
+//   findById, 
+//   insert, 
+//   remove, 
+//   update
+// }
+
+function find() {
   return db("items")
-  .select("name", "price", "category")
+    .orderBy("id", "asc")
 }
 
 function findBy(filter) {
-  return db("items").where(filter)
+  return db("items")
+    .where(filter)
 }
 
-function findById(id) {
-  return db("items")
-  .select("name", "price", "category")
-  .where({id})
-  .first()
+async function findById(id) {
+  const item = await db("items").where({ id })
+  return item.length === 0 ? null : item
 }
 
 async function insert(item) {
-  return await db("items")
-  .insert(item)
-  .returning(["id", "name"])
+  const [id] = await db("items").insert(item)
+  return findById(id)
 }
 
 function remove(id) {
   return db("items")
-  .where({id})
-  .del()
+    .where({ id })
+    .del()
 }
 
-function update(changes, id) {
-  return db("items")
-  .where({id})
-  .update(changes)
+async function update(id, updates) {
+  await db("items")
+    .where({ id })
+    .update(updates)
+    
+  return findById(id)
 }
 
 module.exports = {
-  find, 
-  findBy, 
-  findById, 
-  insert, 
-  remove, 
+  find,
+  findBy,
+  findById,
+  insert,
+  remove,
   update
 }
